3cf940b07d2c4e3cad6c40177343e801a346d22e
Begzod Bakhriddinov <bekadeveloper@icloud.com>
2021-07-12 17:47:11 +0500

    Initial Commit

---
+++ Exchange/ExchangeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExchangeApp.swift
+//  Exchange
+//
+//  Created by Beka on 12/07/21.
+//
+
+import SwiftUI
+
+@main
+struct ExchangeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
